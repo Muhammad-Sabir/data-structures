8868dcfd4fc3f638baf27d684d9941ae5bfd745b
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-05-14 07:03:52 +0500

    Add create-duplicate file

---
+++ create-duplicate.py
@@ -0,0 +1,41 @@
+# solution 1
+
+def containsDuplicate(nums):
+    n = len(nums)
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            if (nums[i] == nums[j]):
+                return True
+
+    return False
+
+
+print(containsDuplicate([1, 1, 2, 56]))
+
+# solution 2
+
+def containsDuplicate2(nums):
+    if(len(nums) != len(set(nums))):
+        return True
+
+    return False
+
+
+print(containsDuplicate2([1, 1, 2, 56]))
+
+# solution 3
+
+def containsDuplicate3(nums):
+    templist = []
+
+    for i in range(len(nums)):
+        if(nums[i] in templist):
+            return True
+
+        templist.append(nums[i])
+
+    return False
+
+
+print(containsDuplicate3([1, 1, 2, 56]))
