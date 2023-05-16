4110396df3661dd9b4238d7a43c5520df625bfbc
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-05-16 20:43:14 +0500

    Add another solution of validate-anagram

--- arrays-and-hashmaps/valid-anagram.py
+++ arrays-and-hashmaps/valid-anagram.py
@@ -31,3 +31,18 @@ def isAnagram2(s, t):
     return hashS == hashT
 
 print(isAnagram2("hill", "hlil"))
+
+# solution 3
+
+def isAnagram3(s, t):
+        if len(s) != len(t):
+            return False
+
+        hashS, hashT = {}, {}
+
+        for i in range(len(s)):
+            hashS[s[i]] = 1 + hashS.get(s[i], 0)
+            hashT[t[i]] = 1 + hashT.get(t[i], 0)
+        return hashS == hashT
+
+print(isAnagram3("hill", "hlil"))
